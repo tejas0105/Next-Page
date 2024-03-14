94d3d9cb2ba23cd9028f57a89df2c953ffdac145
tejas0105 <tspathak0105@gmail.com>
2024-03-14 16:23:19 +0530

    device type added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,7 @@ interface Body {
 export default function Home() {
   const [result, setResult] = useState<Item[]>([]);
   const [deviceType, setDeviceType] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const getDateInIso = (timestamp: number): String => {
     let date = new Date(timestamp);
@@ -58,11 +59,20 @@ export default function Home() {
     });
   };
   const getData = async () => {
+    setIsLoading(true);
     const response = await axios.get("http://127.0.0.1:8000/api/finalpage");
     console.log(response);
     setResult(response?.data?.data);
+    setIsLoading(false);
     // console.log(result);
     // console.log({ lat: result.latitude, long: result.longitude });
+  };
+
+  const handleResize = () => {
+    const { innerWidth, innerHeight } = window;
+    if (innerWidth <= 768) setDeviceType("mobile");
+    else if (innerWidth > 768 && innerWidth <= 1024) setDeviceType("tablet");
+    else setDeviceType("desktop");
   };
 
   const sendCoordinates = async () => {
@@ -77,7 +87,7 @@ export default function Home() {
         const body: Body = {
           lat: result?.latitude,
           long: result?.longitude,
-          deviceType: "Desktop",
+          deviceType: deviceType,
           ip: ip,
         };
         await axios.post("http://127.0.0.1:8000/api/getCoord", body);
@@ -87,10 +97,10 @@ export default function Home() {
           const ipResp = await axios.get("https://api.ipify.org?format=json");
           const ip = ipResp?.data;
           await axios.post("http://127.0.0.1:8000/api/handlenulllocation", {
-            code: error.code,
-            ip: ip.ip,
-            date: getDateInIso(Date.now()),
-            allowed: false,
+            lat: null,
+            long: null,
+            deviceType: deviceType,
+            ip: ip,
           });
         }
       }
@@ -101,14 +111,45 @@ export default function Home() {
 
   useEffect(() => {
     getData();
-    sendCoordinates();
-    // const width = ref;
-    // console.log(ref.current.clientWidth);
+    handleResize();
   }, []);
 
   // useEffect(() => {
   //   if (result) console.log(result);
   // }, [result]);
+
+  useEffect(() => {
+    if (deviceType) {
+      sendCoordinates();
+    }
+  }, [deviceType]);
+
+  if (isLoading) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center  h-screen p-4">
       <div className="mt-16 sm:w-[35rem] md:w-[44rem] lg:w-[55rem] h-full">
